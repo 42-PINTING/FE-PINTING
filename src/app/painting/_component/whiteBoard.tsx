ce283e4aa8910f008b9a6adc3073e25cb1182928
jaeywon <dnsjwodyd@gmail.com>
2024-04-23 02:58:05 +0900

    [Feat]  줌인 아웃 기능구현

--- src/app/painting/_component/whiteBoard.tsx
+++ src/app/painting/_component/whiteBoard.tsx
@@ -1,12 +1,19 @@
 'use client';
 import { fabric } from 'fabric';
 import { useEffect, useRef, useState } from 'react';
+import { useCanvasPanning } from '../_hooks/useCanvasPanning';
+import TriangleTool from './triangleTool';
 
 export default function WhiteBoard() {
-  const canvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
-  const [activeTool, setActiveTool] = useState('select');
+  const [activeTool, setActiveTool] = useState('pen');
 
+  const [initialCanvasSize, setInitialCanvasSize] = useState({
+    width: 1000,
+    height: 500,
+  });
+  useCanvasPanning(canvas, activeTool);
   useEffect(() => {
     if (!canvasRef.current || !canvas) return;
 
@@ -17,6 +24,9 @@ export default function WhiteBoard() {
 
       case 'pen':
         handlePenTool();
+        break;
+
+      case 'move':
         break;
     }
   }, [activeTool]);
@@ -29,19 +39,73 @@ export default function WhiteBoard() {
     canvas.isDrawingMode = true;
   };
 
+  // useEffect(() => {
+  //   const newCanvas = new fabric.Canvas(canvasRef.current, {
+  //     width: 1000,
+  //     height: 500,
+  //   });
+  //   setCanvas(newCanvas);
+  //   newCanvas.on('mouse:wheel', function (opt) {
+  //     const delta = opt.e.deltaY;
+  //     let zoom = newCanvas.getZoom();
+  //     zoom *= 0.999 ** delta;
+  //     if (zoom > 20) zoom = 20;
+  //     if (zoom < 0.01) zoom = 0.01;
+  //     newCanvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+  //     opt.e.preventDefault();
+  //     opt.e.stopPropagation();
+  //   });
+  //   const resizeCanvas = () => {
+  //     newCanvas.setWidth(window.innerWidth);
+  //     newCanvas.setHeight(window.innerHeight);
+  //     newCanvas.renderAll();
+  //   };
+  //   window.addEventListener('resize', resizeCanvas);
+  //   resizeCanvas();
+
+  //   return () => {
+  //     newCanvas.dispose();
+  //     window.removeEventListener('resize', resizeCanvas);
+  //   };
+  // }, []);
+
   useEffect(() => {
     const newCanvas = new fabric.Canvas(canvasRef.current, {
-      width: 2000,
-      height: 1000,
+      width: initialCanvasSize.width,
+      height: initialCanvasSize.height,
     });
     setCanvas(newCanvas);
+    newCanvas.on('mouse:wheel', function (opt) {
+      const delta = opt.e.deltaY;
+      let zoom = newCanvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 0.01) zoom = 0.01;
+      newCanvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
+    });
+
+    const resizeCanvas = () => {
+      // 브라우저 창 크기가 초기 설정 값보다 클 때만 새로운 크기를 적용합니다.
+      const newWidth = Math.max(window.innerWidth, initialCanvasSize.width);
+      const newHeight = Math.max(window.innerHeight, initialCanvasSize.height);
+      newCanvas.setWidth(newWidth);
+      newCanvas.setHeight(newHeight);
+      newCanvas.renderAll();
+    };
+    window.addEventListener('resize', resizeCanvas);
+
     return () => {
       newCanvas.dispose();
+      window.removeEventListener('resize', resizeCanvas);
     };
   }, []);
+
   return (
     <>
       <canvas style={{ border: '1px solid red' }} ref={canvasRef} />
+      {canvas && <TriangleTool canvas={canvas} />}{' '}
       <button
         style={{ width: '48px', height: '48px', border: '1px solid black' }}
         onClick={() => setActiveTool('select')}
@@ -55,6 +119,13 @@ export default function WhiteBoard() {
         disabled={activeTool === 'pen'}
       >
         펜
+      </button>
+      <button
+        style={{ width: '48px', height: '48px', border: '1px solid black' }}
+        onClick={() => setActiveTool('move')}
+        disabled={activeTool === 'move'}
+      >
+        이동
       </button>
     </>
   );
