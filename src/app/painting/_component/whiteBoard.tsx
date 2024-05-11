d3622cf79c357f9bfc9fb1a8945cf6543adafcf9
jaeywon <dnsjwodyd@gmail.com>
2024-05-11 20:25:04 +0900

    [Feat] 상태관리 변경

--- src/app/painting/_component/whiteBoard.tsx
+++ src/app/painting/_component/whiteBoard.tsx
@@ -1,19 +1,20 @@
 'use client';
 import { fabric } from 'fabric';
 import { useEffect, useRef, useState } from 'react';
-import { useCanvasPanning } from '../_hooks/useCanvasPanning';
+import handleMoveTool from './penPenning';
 import TriangleTool from './triangleTool';
+import { useRecoilState } from 'recoil';
+import { toolState } from '@/common/atoms/penAtoms';
 
 export default function WhiteBoard() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [canvas, setCanvas] = useState<fabric.Canvas | null>(null);
-  const [activeTool, setActiveTool] = useState('pen');
-
+  const [activeTool, setActiveTool] = useRecoilState(toolState);
+  const [showTriangleTool, setShowTriangleTool] = useState(false);
   const [initialCanvasSize, setInitialCanvasSize] = useState({
     width: 1000,
     height: 500,
   });
-  useCanvasPanning(canvas, activeTool);
   useEffect(() => {
     if (!canvasRef.current || !canvas) return;
 
@@ -27,6 +28,7 @@ export default function WhiteBoard() {
         break;
 
       case 'move':
+        handleMoveTool(canvas);
         break;
     }
   }, [activeTool]);
@@ -34,40 +36,17 @@ export default function WhiteBoard() {
   const handleSelectTool = () => {
     canvas.isDrawingMode = false;
   };
+
   const handlePenTool = () => {
     canvas.freeDrawingBrush.width = 10;
     canvas.isDrawingMode = true;
   };
 
-  // useEffect(() => {
-  //   const newCanvas = new fabric.Canvas(canvasRef.current, {
-  //     width: 1000,
-  //     height: 500,
-  //   });
-  //   setCanvas(newCanvas);
-  //   newCanvas.on('mouse:wheel', function (opt) {
-  //     const delta = opt.e.deltaY;
-  //     let zoom = newCanvas.getZoom();
-  //     zoom *= 0.999 ** delta;
-  //     if (zoom > 20) zoom = 20;
-  //     if (zoom < 0.01) zoom = 0.01;
-  //     newCanvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-  //     opt.e.preventDefault();
-  //     opt.e.stopPropagation();
-  //   });
-  //   const resizeCanvas = () => {
-  //     newCanvas.setWidth(window.innerWidth);
-  //     newCanvas.setHeight(window.innerHeight);
-  //     newCanvas.renderAll();
-  //   };
-  //   window.addEventListener('resize', resizeCanvas);
-  //   resizeCanvas();
-
-  //   return () => {
-  //     newCanvas.dispose();
-  //     window.removeEventListener('resize', resizeCanvas);
-  //   };
-  // }, []);
+  const handleTriangleTool = () => {
+    canvas.selection = false;
+    canvas.isDrawingMode = false;
+    setShowTriangleTool(!showTriangleTool);
+  };
 
   useEffect(() => {
     const newCanvas = new fabric.Canvas(canvasRef.current, {
@@ -105,25 +84,25 @@ export default function WhiteBoard() {
   return (
     <>
       <canvas style={{ border: '1px solid red' }} ref={canvasRef} />
-      {canvas && <TriangleTool canvas={canvas} />}{' '}
+      {canvas && <TriangleTool canvas={canvas} />}
       <button
         style={{ width: '48px', height: '48px', border: '1px solid black' }}
         onClick={() => setActiveTool('select')}
-        disabled={activeTool === 'select'}
+        disabled={activeTool === 'select'} // 선택 도구가 활성화되어 있으면 비활성화
       >
         선택
       </button>
       <button
         style={{ width: '48px', height: '48px', border: '1px solid black' }}
         onClick={() => setActiveTool('pen')}
-        disabled={activeTool === 'pen'}
+        disabled={activeTool === 'pen'} // 펜 도구가 활성화되어 있으면 비활성화
       >
         펜
       </button>
       <button
         style={{ width: '48px', height: '48px', border: '1px solid black' }}
         onClick={() => setActiveTool('move')}
-        disabled={activeTool === 'move'}
+        disabled={activeTool === 'move'} // 이동 도구가 활성화되어 있으면 비활성화
       >
         이동
       </button>
