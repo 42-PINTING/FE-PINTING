dfcf0490abfa6d20d5c939f4339c25434d57f0b2
conteng <contemplation.person@gmail.com>
2024-05-21 01:28:43 +0900

    feat: button을 별도 컴포넌트로 만들었음

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,48 @@
 import Image from 'next/image';
 import Sign from './_component/Sign';
+
 import styles from './_styles/Home.module.scss';
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next';
+import { server } from '../mocks/node.js';
+import { instance } from '@/utils/axios';
+import Oauth from './_component/Oauth';
 
 server.listen();
 
-export default async function main() {
+//TODO: server side Props 만들기
+
+// type Main = {
+//   page: string;
+//   isError: boolean;
+// };
+
+// export const getServerSideProps = (async () => {
+//   let page;
+//   let isError;
+
+//   await instance
+//     .get(`${process.env.NEXT_PUBLIC_SERVER_LOGIN_ENDPOINT}`)
+//     .then((res) => {
+//       page = res.data;
+//       isError = false;
+//     })
+//     .catch((err) => {
+//       page = err;
+//       isError = true;
+//     });
+
+//   return { props: { page, isError } };
+// }) satisfies GetServerSideProps<{ main: Main }>;
+
+export default function main() {
   return (
     <div className={styles.homeLayout}>
-      <div className={styles.leftLogicBox}>
+      <div className={styles.titleLoginBox}>
         <h1 className={styles.homeTitle}>PINTING</h1>
         <Sign />
         <div className={styles.oauthBtnGroup}>
-          <button className={styles.oauthBtn} type='button'>
-            Oauth google
-          </button>
-          <button className={styles.oauthBtn} type='button'>
-            Oauth kakao
-          </button>
+          <Oauth.Google />
+          <Oauth.Kakao />
         </div>
       </div>
       <Image
