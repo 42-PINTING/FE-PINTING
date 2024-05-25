da1fe99b02cd6c63fb0457a0abd3fcbc6cc78923
jaeywon <dnsjwodyd@gmail.com>
2024-05-26 05:33:42 +0900

    fix: :bug: webpack에서 canvas모듈찾지못하는버그수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    // node-loader 설정 추가
+    config.module.rules.push({
+      test: /\.node$/,
+      use: 'node-loader',
+    });
+
+    // Node.js 네이티브 모듈을 번들에서 제외
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push('canvas');
+    }
+
+    // 확장자 처리 추가
+    config.resolve.extensions.push('.node');
+
+    return config;
+  },
+};
 
 export default nextConfig;
