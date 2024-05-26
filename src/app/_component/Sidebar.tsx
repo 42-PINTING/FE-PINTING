6394e5ef3305163b2ba36c56710c53b053001385
conteng <contemplation.person@gmail.com>
2024-05-27 02:34:54 +0900

    feat: 함수 분리 및 css 추가

--- src/app/_component/Sidebar.tsx
+++ src/app/_component/Sidebar.tsx
@@ -1,48 +1,87 @@
 import Link from 'next/link';
 import styles from '../_styles/Sidebar.module.scss';
-import Title from '@/globalComponent/Titile';
+import Title from '../../globalComponent/Titile';
+import { FaGithub } from 'react-icons/fa';
+import { BsEnvelopePaperHeart } from 'react-icons/bs';
+import { FaPen } from 'react-icons/fa';
+import { FaUser } from 'react-icons/fa';
 
 /**
  * 그리기 == 개인 그림
  * 그림편지 == 게시판
  */
 const navPersonalItems = [
-  { href: '/painting', text: '그리기' },
-  { href: '/', text: '그림 편지' },
-  { href: '/user', text: '유저' },
+  { href: '/painting', text: '그리기', icon: <FaPen /> },
+  { href: '/', text: '그림 편지', icon: <BsEnvelopePaperHeart /> },
 ];
 
-const navCoItems = [{ href: '/coDrawing', text: '공유 그림' }];
+const navCoItems = [
+  { href: '/coDrawing', text: '공유 그림', icon: <FaUser /> },
+];
+
+const navPrivateItems = [{ href: '/user', text: '유저', icon: <FaUser /> }];
 
 export default function Sidebar() {
   return (
     <nav className={styles.sidebar}>
       <h2 className={styles.sidebarTitle}>
-        <Link href='/' className={styles.sidebarLink}>
-          <Title.basic />
-        </Link>
+        <Title.basic />
       </h2>
       <ul className={styles.sidebarList}>
-        {navPersonalItems.map((item) => (
-          <li key={item.href} className={styles.sidebarItem}>
-            <Link href={item.href} className={styles.sidebarLink}>
-              {item.text}
-            </Link>
-          </li>
-        ))}
-        <hr />
-        {navCoItems.map((item) => (
-          <li key={item.href} className={styles.sidebarItem}>
-            <Link href={item.href} className={styles.sidebarLink}>
-              {item.text}
-            </Link>
-          </li>
-        ))}
+        <PersonalItems />
+        <hr className={styles.line} />
+        <CoItems />
+        <hr className={styles.line} />
+        <PrivateItems />
       </ul>
+      <FooterItems />
+    </nav>
+  );
+}
+
+function PersonalItems() {
+  return navPersonalItems.map((item) => (
+    <li key={item.href} className={styles.sidebarItem}>
+      <Link href={item.href} className={styles.sidebarLink}>
+        <div className={styles.itemIcon}>{item.icon}</div>
+        <div className={styles.itemText}>{item.text}</div>
+      </Link>
+    </li>
+  ));
+}
+
+function CoItems() {
+  return navCoItems.map((item) => (
+    <li key={item.href} className={styles.sidebarItem}>
+      <Link href={item.href} className={styles.sidebarLink}>
+        <div className={styles.itemIcon}>{item.icon}</div>
+        <div className={styles.itemText}>{item.text}</div>
+      </Link>
+    </li>
+  ));
+}
+
+function PrivateItems() {
+  return navPrivateItems.map((item) => (
+    <li key={item.href} className={styles.sidebarItem}>
+      <Link href={item.href} className={styles.sidebarLink}>
+        <div className={styles.itemIcon}>{item.icon}</div>
+        <div className={styles.itemText}>{item.text}</div>
+      </Link>
+    </li>
+  ));
+}
+
+function FooterItems() {
+  return (
+    <Link href='https://github.com/42-PINTING' className={styles.footerWrapper}>
       <footer className={styles.footer}>
+        <div className={styles.copyright}>
+          <FaGithub />
+        </div>
         <div className={styles.copyright}>copyright</div>
         <div className={styles.copyright}>reserved</div>
       </footer>
-    </nav>
+    </Link>
   );
 }
