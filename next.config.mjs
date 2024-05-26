168dfefddccfcf35b192ce4e220a3bc97b3cfb97
jaeywon <dnsjwodyd@gmail.com>
2024-05-27 00:32:24 +0900

    fix: :art: canvas모듈 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // node-loader 설정 추가
-    config.module.rules.push({
-      test: /\.node$/,
-      use: 'node-loader',
-    });
-
-    // Node.js 네이티브 모듈을 번들에서 제외
-    if (isServer) {
-      config.externals = config.externals || [];
-      config.externals.push('canvas');
-    }
-
-    // 확장자 처리 추가
-    config.resolve.extensions.push('.node');
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
