d1bdb784cccd7ad336cbf87444e2d7ac1fa9688f
jaeywon <dnsjwodyd@gmail.com>
2024-05-27 07:14:12 +0900

    feat: shapeTool 코드 추가

--- src/app/painting/_component/_utils/toolIndex.tsx
+++ src/app/painting/_component/_utils/toolIndex.tsx
@@ -4,13 +4,23 @@ import {
   disablePanningTool,
 } from '@/app/painting/_component/_utils/panningTool';
 import selectionTool from '@/app/painting/_component/_utils/selectionTool';
+import {
+  triangleTool,
+  rectangleTool,
+} from '@/app/painting/_component/_utils/shapeTool';
 
 export const Pen = {
   basic: basicPen,
 };
 
+export const Shapes = {
+  triangle: triangleTool,
+  rectangle: rectangleTool,
+};
+
 export const Tool = {
   pen: Pen,
+  shape: Shapes,
   selection: selectionTool,
   panning: panningTool,
   disablePanning: disablePanningTool,
