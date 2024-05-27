b7cfcd86a147d509141002630049ab4ceb6da07c
conteng <contemplation.person@gmail.com>
2024-05-28 00:26:02 +0900

    feat: sidebar layout 완성
    온클릭 이벤트나 햄버거버튼은 추후 고민 사항

--- src/app/_component/Sidebar.tsx
+++ src/app/_component/Sidebar.tsx
@@ -1,10 +1,9 @@
 import Link from 'next/link';
 import styles from '../_styles/Sidebar.module.scss';
 import Title from '../../globalComponent/Titile';
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaHeart, FaPen, FaUser, FaSlideshare } from 'react-icons/fa';
 import { BsEnvelopePaperHeart } from 'react-icons/bs';
-import { FaPen } from 'react-icons/fa';
-import { FaUser } from 'react-icons/fa';
+import { LuTreeDeciduous } from 'react-icons/lu';
 
 /**
  * 그리기 == 개인 그림
@@ -12,27 +11,38 @@ import { FaUser } from 'react-icons/fa';
  */
 const navPersonalItems = [
   { href: '/painting', text: '그리기', icon: <FaPen /> },
-  { href: '/', text: '그림 편지', icon: <BsEnvelopePaperHeart /> },
+  {
+    href: '/',
+    text: '그림 편지',
+    icon: <BsEnvelopePaperHeart color='orange' />,
+  },
 ];
 
 const navCoItems = [
-  { href: '/coDrawing', text: '공유 그림', icon: <FaUser /> },
+  {
+    href: '/coDrawing',
+    text: '공유 그림',
+    icon: <FaSlideshare color='#008000' />,
+  },
 ];
 
-const navPrivateItems = [{ href: '/user', text: '유저', icon: <FaUser /> }];
+const navPrivateItems = [
+  { href: '/user', text: '유저', icon: <FaUser color='gray' /> },
+];
 
 export default function Sidebar() {
   return (
-    <nav className={styles.sidebar}>
-      <h2 className={styles.sidebarTitle}>
+    <nav className={styles.nav}>
+      <h2 className={styles.title}>
         <Title.basic />
       </h2>
-      <ul className={styles.sidebarList}>
+      <ul className={styles.list}>
         <PersonalItems />
         <hr className={styles.line} />
         <CoItems />
         <hr className={styles.line} />
         <PrivateItems />
+        <AttendanceBox />
       </ul>
       <FooterItems />
     </nav>
@@ -41,8 +51,8 @@ export default function Sidebar() {
 
 function PersonalItems() {
   return navPersonalItems.map((item) => (
-    <li key={item.href} className={styles.sidebarItem}>
-      <Link href={item.href} className={styles.sidebarLink}>
+    <li key={item.href} className={styles.item} id={`${item.text}`}>
+      <Link href={item.href} className={styles.link}>
         <div className={styles.itemIcon}>{item.icon}</div>
         <div className={styles.itemText}>{item.text}</div>
       </Link>
@@ -52,8 +62,8 @@ function PersonalItems() {
 
 function CoItems() {
   return navCoItems.map((item) => (
-    <li key={item.href} className={styles.sidebarItem}>
-      <Link href={item.href} className={styles.sidebarLink}>
+    <li key={item.href} className={styles.item}>
+      <Link href={item.href} className={styles.link}>
         <div className={styles.itemIcon}>{item.icon}</div>
         <div className={styles.itemText}>{item.text}</div>
       </Link>
@@ -63,13 +73,27 @@ function CoItems() {
 
 function PrivateItems() {
   return navPrivateItems.map((item) => (
-    <li key={item.href} className={styles.sidebarItem}>
-      <Link href={item.href} className={styles.sidebarLink}>
+    <li key={item.href} className={styles.item}>
+      <Link href={item.href} className={styles.link}>
         <div className={styles.itemIcon}>{item.icon}</div>
         <div className={styles.itemText}>{item.text}</div>
       </Link>
     </li>
   ));
+}
+
+function AttendanceBox() {
+  return (
+    <div className={styles.attendanceBox}>
+      <div>출석체크</div>
+      <div>
+        <LuTreeDeciduous color='#008000' />
+      </div>
+      <div>
+        <FaHeart color='red' />
+      </div>
+    </div>
+  );
 }
 
 function FooterItems() {
