5ce1dbce5f732bec97cd385c77c730959871dbef
conteng <contemplation.person@gmail.com>
2024-05-31 01:25:05 +0900

    refactor: 폴더 디렉토리 구조 변경
    _global*, mock 등

--- src/mocks/node.js
+++ src/mocks/node.js
@@ -1,10 +1,4 @@
-// src/mocks/node.js
 import { setupServer } from 'msw/node';
-import { checkLogin, randomeImage } from './Login';
-// import { validateEmail } from './SignUp.js';
+import { successSignout } from './global/signInbutton.ts';
 
-export const server = setupServer(
-  ...checkLogin,
-  ...randomeImage
-  // ...validateEmail
-);
+export const server = setupServer(...successSignout);
