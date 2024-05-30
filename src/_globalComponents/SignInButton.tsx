ec1ea90bf5210ef974ff1bbac6b069fdd98b805f
conteng <contemplation.person@gmail.com>
2024-05-31 01:07:55 +0900

    feat: signInButton css 삽입

--- src/_globalComponents/SignInButton.tsx
+++ src/_globalComponents/SignInButton.tsx
@@ -3,12 +3,17 @@
 import Link from 'next/link';
 import { useRecoilState } from 'recoil';
 import { signInState } from '@/_globalAtoms/signIn';
+import styles from '@/_globalStyles/SignInButton.module.scss';
 
 export default function SignInButton() {
   const [isPintingSignIn, setIsPintingSignIn] = useRecoilState(signInState);
 
   if (!isPintingSignIn) {
-    return <Link href='/signIn'>로그인</Link>;
+    return (
+      <Link href='/signIn' className={styles.signIn}>
+        로그인
+      </Link>
+    );
   }
 
   const handleLogout = () => {
