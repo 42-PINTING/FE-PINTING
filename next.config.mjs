89e87870cc8203daedbd98e30c5db59ebbe892b5
jaeywon <dnsjwodyd@gmail.com>
2024-05-31 19:34:14 +0900

    chore: canvas 오류 발생 환경설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['fabric'],
+  },
+  webpack: (config, { webpack }) => {
+    config.externals.push({ canvas: 'commonjs canvas' });
+    return config;
+  },
 };
 
 export default nextConfig;
