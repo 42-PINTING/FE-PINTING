a40de2c03b4924247c5e0e5aaf3848a75b5013bd
jaeywon <dnsjwodyd@gmail.com>
2024-06-09 02:48:29 +0900

    feat: 캔버스 히스토리 저장을위한 아콤생성

---
+++ src/app/painting/_atoms/canvasAtoms.tsx
@@ -0,0 +1,11 @@
+import { atom } from 'recoil';
+
+export const canvasHistoryState = atom<fabric.Object[][]>({
+  key: 'canvasHistoryState',
+  default: [],
+});
+
+export const canvasIndexState = atom<number>({
+  key: 'canvasIndexState',
+  default: -1,
+});
