a4e5c893b70ad39b509f0dbdf554c9b2a142d806
conteng <contemplation.person@gmail.com>
2024-06-11 17:16:11 +0900

    refactor: 쿠키는 자동으로 넣어주기 때문에 쿠키에 접근할 필요가 없음

--- src/global/utils/axios.ts
+++ src/global/utils/axios.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosError } from 'axios';
-import { cookies } from 'next/headers.js';
 
 const baseURL = `${process.env.NEXT_PUBLIC_SERVER_ENDPOINT}`;
 const instance = axios.create({ baseURL });
@@ -7,21 +6,6 @@ const instance = axios.create({ baseURL });
 // TODO : 추후 server axios instance와 server instance를 분리해야함
 instance.interceptors.request.use(
   function setConfig(config) {
-    let pintingAccessToken = '';
-
-    if (typeof window === 'undefined') {
-      pintingAccessToken = cookies().get('pintingAccessToken')?.value ?? '';
-    } else {
-      pintingAccessToken = document.cookie.replace(
-        /(?:(?:^|.*;\s*)pintingAccessToken\s*\=\s*([^;]*).*$)|^.*$/,
-        '$1'
-      );
-    }
-
-    if (config.headers) {
-      config.headers['Content-Type'] = 'application/json';
-      config.headers['Authorization'] = `${pintingAccessToken}`;
-    }
     config.withCredentials = true;
 
     return config;
