9f8b08915131ca4b8be59ff2455496ccdfa1b9a7
jaeywon <dnsjwodyd@gmail.com>
2024-06-18 16:23:43 +0900

    feat: 리사이징 기능 추가

--- src/app/painting/_component/whiteBoard.tsx
+++ src/app/painting/_component/whiteBoard.tsx
@@ -5,18 +5,25 @@ import React, { useEffect, useRef } from 'react';
 const WhiteBoard = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
+  const resizeCanvas = (canvas) => {
+    const context = canvas.getContext('2d');
+    canvas.width = window.innerWidth * (3 / 5);
+    canvas.height = window.innerHeight * (3 / 5);
+    context.fillStyle = '#ffffff'; // 배경색 설정
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  };
+
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas) {
-      const context = canvas.getContext('2d');
-      if (context) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        context.fillStyle = '#ffffff'; // 배경색 설정
-        context.fillRect(0, 0, canvas.width, canvas.height);
-      }
+      resizeCanvas(canvas);
+      window.addEventListener('resize', () => resizeCanvas(canvas));
     }
+    return () => {
+      window.removeEventListener('resize', () => resizeCanvas(canvas));
+    };
   }, []);
+
   return <canvas ref={canvasRef} style={{ border: '1px solid #000' }}></canvas>;
 };
 
