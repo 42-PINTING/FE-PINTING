181c75dafcb07ca6ae7fdb0a8c847e0ddb9bf94d
jaeywon <dnsjwodyd@gmail.com>
2024-06-18 16:25:41 +0900

    feat: 타입명시적으로 적용

--- src/app/painting/_component/whiteBoard.tsx
+++ src/app/painting/_component/whiteBoard.tsx
@@ -1,27 +1,29 @@
 'use client';
-
 import React, { useEffect, useRef } from 'react';
 
-const WhiteBoard = () => {
+const WhiteBoard: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-  const resizeCanvas = (canvas) => {
+  const resizeCanvas = (canvas: HTMLCanvasElement) => {
     const context = canvas.getContext('2d');
-    canvas.width = window.innerWidth * (3 / 5);
-    canvas.height = window.innerHeight * (3 / 5);
-    context.fillStyle = '#ffffff'; // 배경색 설정
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    if (context) {
+      canvas.width = window.innerWidth * (3 / 5);
+      canvas.height = window.innerHeight * (3 / 5);
+      context.fillStyle = '#ffffff'; // 배경색 설정
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    }
   };
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas) {
       resizeCanvas(canvas);
-      window.addEventListener('resize', () => resizeCanvas(canvas));
+      const handleResize = () => resizeCanvas(canvas);
+      window.addEventListener('resize', handleResize);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
     }
-    return () => {
-      window.removeEventListener('resize', () => resizeCanvas(canvas));
-    };
   }, []);
 
   return <canvas ref={canvasRef} style={{ border: '1px solid #000' }}></canvas>;
