50897f88340cb9052862e9d40a68a7bdaa31a5d6
jaeywon <dnsjwodyd@gmail.com>
2024-06-18 15:05:25 +0900

    fix: 충돌해결

--- src/app/painting/_component/_utils/toolIndex.tsx
+++ src/app/painting/_component/_utils/toolIndex.tsx
@@ -1,9 +1,7 @@
 'use client';
-<<<<<<< HEAD
+
 import { basicPen, eraserTool } from '@/app/painting/_component/_utils/penTool';
-=======
-import { BasicPen } from '@/app/painting/_component/_utils/penTool';
->>>>>>> 40ff491be913f17140ce57e812026e114a08e46f
+
 import {
   PanningTool,
   DisablePanningTool,
@@ -12,12 +10,8 @@ import selectionTool from '@/app/painting/_component/_utils/selectionTool';
 import { TriangleTool, RectangleTool, CircleTool } from './shapeTool';
 
 export const Pen = {
-<<<<<<< HEAD
   basic: basicPen,
   eraser: eraserTool,
-=======
-  basic: BasicPen,
->>>>>>> 40ff491be913f17140ce57e812026e114a08e46f
 };
 
 export const Shapes = {
