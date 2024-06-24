b9783194cea38fc9286c3e7f92f2fba4c967571a
jaeywon <dnsjwodyd@gmail.com>
2024-06-24 17:46:46 +0900

    feat: fabric.js로 그림판 캔버스 빌드

--- src/app/painting/_component/whiteBoard.tsx
+++ src/app/painting/_component/whiteBoard.tsx
@@ -1,86 +1,35 @@
 'use client';
-import React, { useEffect, useRef, useState } from 'react';
-import { useRecoilState } from 'recoil';
-import { toolState } from '@/app/painting/_atoms/penAtoms';
-import { SwitchTool } from './_utils/switchTool';
+import { useEffect, useRef } from 'react';
 
-const WhiteBoard: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [tool, setTool] = useRecoilState(toolState);
-
-  const resizeCanvas = (canvas: HTMLCanvasElement) => {
-    const context = canvas.getContext('2d');
-    if (context) {
-      const originalWidth = canvas.width;
-      const originalHeight = canvas.height;
-      const savedContent = context.getImageData(
-        0,
-        0,
-        originalWidth,
-        originalHeight
-      );
-
-      // 캔버스 크기 조정
-      canvas.width = window.innerWidth * (3 / 5);
-      canvas.height = window.innerHeight * (3 / 5);
-      context.fillStyle = '#ffffff'; // 배경색 설정
-      context.fillRect(0, 0, canvas.width, canvas.height);
-
-      // 저장된 내용을 새로운 크기에 맞춰 다시 그리기
-      const tempCanvas = document.createElement('canvas');
-      tempCanvas.width = originalWidth;
-      tempCanvas.height = originalHeight;
-      const tempContext = tempCanvas.getContext('2d');
-      if (tempContext) {
-        tempContext.putImageData(savedContent, 0, 0);
-        context.drawImage(
-          tempCanvas,
-          0,
-          0,
-          originalWidth,
-          originalHeight,
-          0,
-          0,
-          canvas.width,
-          canvas.height
-        );
-      }
-    }
-  };
+const WhiteBoard = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      resizeCanvas(canvas);
-      const handleResize = () => resizeCanvas(canvas);
-      window.addEventListener('resize', handleResize);
-      return () => {
-        window.removeEventListener('resize', handleResize);
+    const loadFabric = async () => {
+      const fabricScript = document.createElement('script');
+      fabricScript.src = '/fabric.min.js';
+      fabricScript.onload = () => {
+        const fabric = (window as any).fabric;
+        const canvas = new fabric.Canvas(canvasRef.current);
+
+        // 캔버스를 브라우저 창의 3분의 2 크기로 설정
+        const width = (window.innerWidth * 2) / 3;
+        const height = (window.innerHeight * 2) / 3;
+        canvas.setWidth(width);
+        canvas.setHeight(height);
       };
+      document.head.appendChild(fabricScript);
+    };
+
+    const canvasElement = canvasRef.current;
+    if (canvasElement) {
+      canvasElement.style.border = '1px solid black';
     }
+
+    loadFabric();
   }, []);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    if (canvas) {
-      canvas.style.cursor = tool === 'pen' ? 'crosshair' : 'default';
-    }
-  }, [tool]);
-
-  const handleToolChange = (selectedTool: string) => {
-    setTool(selectedTool);
-  };
-
-  return (
-    <>
-      <SwitchTool
-        handleToolChange={handleToolChange}
-        tool={tool}
-        canvas={canvasRef.current}
-      />
-      <canvas ref={canvasRef} style={{ border: '1px solid #000' }}></canvas>
-    </>
-  );
+  return <canvas ref={canvasRef} id='whiteBoard'></canvas>;
 };
 
 export default WhiteBoard;
