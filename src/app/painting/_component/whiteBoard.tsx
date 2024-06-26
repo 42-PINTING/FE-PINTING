2d6ae36a4be757b16ba975259537cd858ae2afca
jaeywon <dnsjwodyd@gmail.com>
2024-06-26 16:32:22 +0900

    feat: 기본적인 캔버스 토대 구현

--- src/app/painting/_component/whiteBoard.tsx
+++ src/app/painting/_component/whiteBoard.tsx
@@ -1,35 +1,26 @@
 'use client';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { fabric } from 'fabric';
 
 const WhiteBoard = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef(null);
+  const [canvas, setCanvas] = useState<fabric.Canvas | null>();
 
   useEffect(() => {
-    const loadFabric = async () => {
-      const fabricScript = document.createElement('script');
-      fabricScript.src = '/fabric.min.js';
-      fabricScript.onload = () => {
-        const fabric = (window as any).fabric;
-        const canvas = new fabric.Canvas(canvasRef.current);
-
-        // 캔버스를 브라우저 창의 3분의 2 크기로 설정
-        const width = (window.innerWidth * 2) / 3;
-        const height = (window.innerHeight * 2) / 3;
-        canvas.setWidth(width);
-        canvas.setHeight(height);
-      };
-      document.head.appendChild(fabricScript);
-    };
-
-    const canvasElement = canvasRef.current;
-    if (canvasElement) {
-      canvasElement.style.border = '1px solid black';
+    if (fabricRef.current) {
+      const newCanvas = new fabric.Canvas('canvas', {
+        width: 500,
+        height: 500,
+      });
+      setCanvas(newCanvas);
     }
-
-    loadFabric();
   }, []);
 
-  return <canvas ref={canvasRef} id='whiteBoard'></canvas>;
+  return (
+    <>
+      <canvas id='canvas'></canvas>
+    </>
+  );
 };
 
 export default WhiteBoard;
