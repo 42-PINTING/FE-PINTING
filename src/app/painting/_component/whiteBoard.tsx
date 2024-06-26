24fd3538a5fca2cbb70e5ba1e3e6d03b588a91b8
jaeywon <dnsjwodyd@gmail.com>
2024-06-26 16:57:59 +0900

    feat: 캔버스 리사이즈 기능 추가

--- src/app/painting/_component/whiteBoard.tsx
+++ src/app/painting/_component/whiteBoard.tsx
@@ -4,21 +4,40 @@ import { fabric } from 'fabric';
 
 const WhiteBoard = () => {
   const fabricRef = useRef(null);
-  const [canvas, setCanvas] = useState<fabric.Canvas | null>();
+  const canvasRef = useRef(null);
 
+  const setCanvasSize = (canvas) => {
+    const width = (window.innerWidth * 2) / 3;
+    const height = (window.innerHeight * 2) / 3;
+    canvas.setWidth(width);
+    canvas.setHeight(height);
+  };
   useEffect(() => {
-    if (fabricRef.current) {
-      const newCanvas = new fabric.Canvas('canvas', {
-        width: 500,
-        height: 500,
-      });
-      setCanvas(newCanvas);
+    if (fabricRef.current && !canvasRef.current) {
+      const newCanvas = new fabric.Canvas(fabricRef.current);
+      canvasRef.current = newCanvas;
+      setCanvasSize(newCanvas);
     }
+
+    const handleResize = () => {
+      if (canvasRef.current) {
+        setCanvasSize(canvasRef.current);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
 
   return (
     <>
-      <canvas id='canvas'></canvas>
+      <canvas
+        id='canvas'
+        ref={fabricRef}
+        style={{ border: '1px solid black' }}
+      ></canvas>
     </>
   );
 };
