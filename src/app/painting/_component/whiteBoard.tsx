155ca0d7a5f0f610109f2aad62a21d5a01beb6fe
jaeywon <dnsjwodyd@gmail.com>
2024-07-11 16:59:36 +0900

    style: 사용하지않는 useState 삭제

--- src/app/painting/_component/whiteBoard.tsx
+++ src/app/painting/_component/whiteBoard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { fabric } from 'fabric';
 import { SwitchTool } from './_utils/switchTool';
 import { useRecoilState } from 'recoil';
@@ -78,4 +78,3 @@ const WhiteBoard = () => {
 };
 
 export default WhiteBoard;
-
