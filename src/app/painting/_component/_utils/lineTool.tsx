d421724054ed4fac7fc7185cc98a4a9f24b7748d
jaeywon <dnsjwodyd@gmail.com>
2024-07-28 05:30:29 +0900

    feat: 직선 생성 기능 구현

---
+++ src/app/painting/_component/_utils/lineTool.tsx
@@ -0,0 +1,46 @@
+import { fabric } from 'fabric';
+
+export const enableLineTool = (canvas: fabric.Canvas) => {
+  let line: fabric.Line | null = null;
+  let isDrawing = false;
+
+  const startDrawing = (options: fabric.IEvent<MouseEvent>) => {
+    isDrawing = true;
+    const pointer = canvas.getPointer(options.e);
+    const points = [pointer.x, pointer.y, pointer.x, pointer.y];
+    line = new fabric.Line(points, {
+      strokeWidth: 2,
+      fill: 'black',
+      stroke: 'black',
+      originX: 'center',
+      originY: 'center',
+    });
+    canvas.add(line);
+  };
+
+  const drawing = (options: fabric.IEvent<MouseEvent>) => {
+    if (!isDrawing || !line) return;
+
+    const pointer = canvas.getPointer(options.e);
+    line.set({
+      x2: pointer.x,
+      y2: pointer.y,
+    });
+    canvas.requestRenderAll();
+  };
+
+  const finishDrawing = () => {
+    isDrawing = false;
+    line = null;
+  };
+
+  canvas.on('mouse:down', startDrawing);
+  canvas.on('mouse:move', drawing);
+  canvas.on('mouse:up', finishDrawing);
+
+  return () => {
+    canvas.off('mouse:down', startDrawing);
+    canvas.off('mouse:move', drawing);
+    canvas.off('mouse:up', finishDrawing);
+  };
+};
