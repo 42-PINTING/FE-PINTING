c59a3e2313fd146faa27012e95fb9972a40c44f6
conteng <contemplation.person@gmail.com>
2024-08-11 00:34:59 +0900

    [Feat] 옛날 데이터 제거

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -7,27 +7,23 @@ import Title from '@/global/components/Titile';
 export default function SignIn() {
   return (
     <div className={styles.signInPanel}>
-     
-    <div className={styles.signInLayout}>
-      <div className={styles.titleLoginBox}>
-        <Title.basic />
-        <h1 className={styles.signInTitle}>Pinting</h1>
-        <SignForm />
-        <div className={styles.oauthBtnGroup}>
-          <Oauth.Google />
+      <div className={styles.signInLayout}>
+        <div className={styles.titleLoginBox}>
+          <Title.basic />
+          <h1 className={styles.signInTitle}>Pinting</h1>
+          <div className={styles.oauthBtnGroup}>
+            <Oauth.Google />
+          </div>
         </div>
+        <Image
+          className={styles.randomUserImage}
+          src='/img/test.svg'
+          alt='random pinting user image'
+          width={500}
+          height={500}
+        />
+        <div className={styles.radiusPanel}></div>
       </div>
-      <Image
-        className={styles.randomUserImage}
-        src='/img/test.svg'
-        alt='random pinting user image'
-        width={500}
-        height={500}
-      />
-       <div className={styles.radiusPanel}>
-    </div>
-   
-    </div>
     </div>
   );
 }
