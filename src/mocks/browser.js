6e7ee32a62e78b0070a1f99f1eef3012d385e677
conteng <contemplation.person@gmail.com>
2024-08-10 23:25:19 +0900

    feat : 로그인 구현

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,6 +1,5 @@
 import { setupWorker } from 'msw/browser';
 import { profile } from './api/me';
-import { signIn } from './api/signIn';
 import { posts } from './api/posts';
 
-export const worker = setupWorker(...profile, ...signIn, ...posts);
+export const worker = setupWorker(...profile, ...posts);
