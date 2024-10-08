ea671f55ed0b913421bde8b44e8e9503e0413a26
wnswo8628 <gbjwein1234@naver.com>
2024-10-09 02:44:51 +0900

    fix: Image 태그 사용시 오류 해결
    #98

--- src/app/coDrawing/_component/myImagePost.tsx
+++ src/app/coDrawing/_component/myImagePost.tsx
@@ -10,7 +10,13 @@ type PostItemProps = {
 const PostItem: React.FC<PostItemProps> = ({ post }) => {
   return (
     <div>
-      <Image className={styles.image} src={post.image} />
+      <Image
+        className={styles.image}
+        src={post.image}
+        width={400} // 원하는 너비
+        height={300} // 원하는 높이
+        alt={post.title}
+      />
       <div className={styles.timeText}>{post.time}</div>
       <div className={styles.titleText}>{post.title}</div>
       <div className={styles.hashTag}>{post.hashTag}</div>
--- src/app/coDrawing/_dummyImage/dataType.tsx
+++ src/app/coDrawing/_dummyImage/dataType.tsx
@@ -1,6 +1,6 @@
 export type Post = {
   id: number; //게시물의 id
-  image: object; //이미지 object
+  image: string; //이미지 object
   title: string; //게시물 제목
   time: string; //작성한 시간
   hashTag: string; //해시태그로 넘어올 내용
