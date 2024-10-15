3fcf1a703ec1cd3b117e0a15de16b8612d16dda1
wnswo8628 <gbjwein1234@naver.com>
2024-10-15 18:53:01 +0900

    feat: :sparkles: 그림 일기 기록 레이아웃
    그림 일기 기록 레이아웃 완성
    
    #102

--- src/app/profile/[user]/page.tsx
+++ src/app/profile/[user]/page.tsx
@@ -5,7 +5,7 @@ import TopProfile from './_component/topProfile';
 import styles from '../[user]/_styles/page.module.scss';
 import { FiEdit2 } from 'react-icons/fi';
 import Image from 'next/image';
-import { FiSettings } from 'react-icons/fi';
+import FollowerContainer from './_component/followerContainer';
 
 const DUMMY_DATA = {
   nickname: 'jugwak',
@@ -20,55 +20,79 @@ const DUMMY_DATA = {
     '/_dummyData/3.png',
     '/_dummyData/4.png',
   ],
+  recordDate: 32,
+  recordDay: 12,
+  heartCount: 20,
+  recordAttendance: 10,
 };
 
 export default function ProfilePage() {
   return (
     <Sidebar>
       <TopProfile />
-      <div className={styles.mainContainer}>
-        <div>
-          <div className={styles.nickname}>
-            {DUMMY_DATA.nickname} <FiEdit2 className={styles.editImage} />
-          </div>
-          <div>{DUMMY_DATA.description}</div>
-        </div>
-        <div className={styles.profileContainer}>
-          <div className={styles.followerContainer}>
-            <Image
-              src={'/_profileImage/profileImage.png'}
-              width={100}
-              height={100}
-              alt={'profileImage'}
-            />
-            <div className={styles.countStyle}>
-              <span className={styles.postCountStyle}>
-                {DUMMY_DATA.postCount}
-              </span>
-              <span className={styles.textStyle}> 게시물</span>
-            </div>
-            <div className={styles.countStyle}>
-              <span className={styles.postCountStyle}>
-                {DUMMY_DATA.follower}
-              </span>
-              <span className={styles.textStyle}> 팔로워</span>
-            </div>
-            <div className={styles.countStyle}>
-              <span className={styles.postCountStyle}>
-                {DUMMY_DATA.following}
-              </span>
-              <span className={styles.textStyle}> 팔로잉</span>
-            </div>
-            <FiSettings />
-          </div>
-          <div className={styles.contour}></div>
+      <div className={styles.holeContainer}>
+        <div className={styles.mainContainer}>
           <div>
-            <span className={styles.bottomTitle}>내가 업로드한 그림</span>
+            <div className={styles.nickname}>
+              {DUMMY_DATA.nickname} <FiEdit2 className={styles.editImage} />
+            </div>
+            <div>{DUMMY_DATA.description}</div>
           </div>
-          <div className={styles.postGird}>
-            {DUMMY_DATA.images.map((post) => (
-              <Image src={post} width={210} height={150} alt={'profileImage'} />
-            ))}
+          <div className={styles.profileContainer}>
+            <FollowerContainer dummyData={DUMMY_DATA} />
+            <div className={styles.contour}></div>
+            <div>
+              <span className={styles.bottomTitle}>내가 업로드한 그림</span>
+            </div>
+            <div className={styles.postGird}>
+              {DUMMY_DATA.images.map((post) => (
+                <Image
+                  src={post}
+                  width={210}
+                  height={150}
+                  alt={'profileImage'}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className={styles.diaryRecordContainer}>
+          <div className={styles.titleContainer}>
+            <span className={styles.recordTitle}>그림 일기 기록</span>
+            <span className={styles.recordDate}>
+              +{DUMMY_DATA.recordDate}일 째
+            </span>
+          </div>
+          <div className={styles.recordBottom}>
+            <Image
+              src={'/_profileImage/Layer 2.svg'}
+              width={70}
+              height={100}
+              alt='dummy'
+            />
+            <div className={styles.recordContainer}>
+              <div className={styles.recordDay}>
+                <span className={styles.dayStyle}>{DUMMY_DATA.recordDay}</span>
+                <span className={styles.marginDay}>일</span>
+                <span>동안 꾸준히 썼어요!</span>
+              </div>
+              <div className={styles.bottonContour}></div>
+              <div className={styles.recordDay}>
+                <span className={styles.dayStyle2}>
+                  {DUMMY_DATA.heartCount}
+                </span>
+                <span className={styles.marginDay}>개</span>
+                <span>하트를 나눠주었어요!</span>
+              </div>
+              <div className={styles.bottonContour}></div>
+              <div className={styles.recordDay}>
+                <span className={styles.dayStyle3}>
+                  {DUMMY_DATA.recordAttendance}
+                </span>
+                <span className={styles.marginDay}>일</span>
+                <span>동안 꾸준히 출석했어요!</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
