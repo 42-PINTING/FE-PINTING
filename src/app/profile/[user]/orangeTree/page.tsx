754162444f090e0908169ae17da1a63b53217c83
wnswo8628 <gbjwein1234@naver.com>
2024-10-22 16:28:32 +0900

    feat: :sparkles: 오렌지나무 페이지 레이아웃 완료
    오렌지나무 레이아웃 완료
    
    #102

--- src/app/profile/[user]/orangeTree/page.tsx
+++ src/app/profile/[user]/orangeTree/page.tsx
@@ -21,42 +21,75 @@ export default function OrangeTree() {
           내 그림으로 돌아가기
         </Link>
         <div className={styles.mainBox}>
-          <div className={styles.treeContainer}>
-            <Image
-              className={styles.imageStyle}
-              src='/treeImg/Level1.png'
-              width={30}
-              height={50}
-              alt={'tree'}
-            />
-            <Image
-              className={styles.imageStyle}
-              src='/treeImg/Level2.png'
-              width={100}
-              height={120}
-              alt={'tree'}
-            />
-            <Image
-              className={styles.imageStyle}
-              src='/treeImg/Level3.png'
-              width={100}
-              height={120}
-              alt={'tree'}
-            />
-            <Image
-              className={styles.imageStyle}
-              src='/treeImg/Level4.png'
-              width={100}
-              height={120}
-              alt={'tree'}
-            />
-            <Image
-              className={styles.imageStyle}
-              src='/treeImg/Level5.png'
-              width={100}
-              height={120}
-              alt={'tree'}
-            />
+          <div className={styles.rightContainer}>
+            <div className={styles.treeContainer}>
+              <Image
+                className={styles.imageStyle}
+                src='/treeImg/Level1.png'
+                width={30}
+                height={50}
+                alt={'tree'}
+              />
+              <Image
+                //   className={styles.imageStyle}
+                src='/treeImg/Level2.png'
+                width={100}
+                height={120}
+                alt={'tree'}
+              />
+              <Image
+                //   className={styles.imageStyle}
+                src='/treeImg/Level3.png'
+                width={100}
+                height={120}
+                alt={'tree'}
+              />
+              <Image
+                //   className={styles.imageStyle}
+                src='/treeImg/Level4.png'
+                width={100}
+                height={120}
+                alt={'tree'}
+              />
+              <Image
+                //   className={styles.imageStyle}
+                src='/treeImg/Level5.png'
+                width={100}
+                height={120}
+                alt={'tree'}
+              />
+            </div>
+            <div className={styles.textContainer}>
+              <span>0~25</span>
+              <span>26~50</span>
+              <span>51~75</span>
+              <span>76~100</span>
+              <span className={styles.plusText}>100+</span>
+            </div>
+            <div className={styles.description}>
+              <span>
+                오렌지를 나눠주며 꾸준히 통해 오렌지 나무를 키워주세요!
+              </span>
+              <span>
+                서로 소중한 관심을 주고 받으면 내 감정을 솔직하게 표현하는데
+                도움이 된다고 해요.
+              </span>
+            </div>
+          </div>
+          <div className={styles.contour}></div>
+          <div className={styles.leftContainer}>
+            <span className={styles.myGrowth}>내 성장 상태</span>
+            <div className={styles.contentContainer}>
+              <Image
+                className={styles.myTreeImage}
+                src='/treeImg/Level2.png'
+                width={100}
+                height={150}
+                alt='tree'
+              />
+              <span>떡잎</span>
+              <span>{DUMMY_DATA.growth}개</span>
+            </div>
           </div>
         </div>
       </div>
