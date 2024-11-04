17f0d384cdd298aeb86e0c7ae01a544208420216
wnswo8628 <gbjwein1234@naver.com>
2024-11-04 18:42:42 +0900

    fix: :sparkles: css 기타 버그 수정
    css 기타 버그 수정
    
    #104

--- src/app/coDrawing/[id]/page.tsx
+++ src/app/coDrawing/[id]/page.tsx
@@ -4,7 +4,6 @@ import Sidebar from '@/global/components/Sidebar';
 import React from 'react';
 import TopContainer from './_component/TopContainer';
 import { useRouter } from 'next/navigation';
-// import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { detailPageState } from '../../../global/atoms/profile';
 import styles from './_styles/page.module.scss';
@@ -15,16 +14,20 @@ const postDetail: React.FC = () => {
 
   const detailData = useRecoilValue(detailPageState);
 
+  function fixButtonHandler() {
+    router.push(`/painting`);
+  }
+
   return (
     <Sidebar>
       <TopContainer />
       <div className={styles.mainContainer}>
         <div className={styles.rightContainer}>
-          {/* <span className={styles.detailTitle}>{detailData.title}</span> */}
-          <span className={styles.detailTitle}>첫 번째 데모 이미지</span>
-          {/* <span>{detailData.time}</span> */}
+          <span className={styles.detailTitle}>{detailData.title}</span>
+          {/* <span className={styles.detailTitle}>첫 번째 데모 이미지</span> */}
           <div className={styles.rightMiddleContainer}>
-            <span className={styles.timeText}>2024.11.23</span>
+            {/* <span className={styles.timeText}>2024.11.23</span> */}
+            <span className={styles.timeText}>{detailData.time}</span>
             <div className={styles.hashTagText}>상쾌해</div>
           </div>
           <Image
@@ -36,11 +39,16 @@ const postDetail: React.FC = () => {
             alt={detailData.title}
           />
           <div className={styles.buttonContainer}>
-            <button className={styles.imageFixButton}>그림 수정</button>
+            <button
+              className={styles.imageFixButton}
+              onClick={fixButtonHandler}
+            >
+              그림 수정
+            </button>
           </div>
         </div>
         <div className={styles.leftContainer}>
-          <div>
+          <div className={styles.leftTopContainer}>
             <span className={styles.commentText}>
               Content_내용_여행가며 보았던 스페인의 산티아고 순례길을 걸으며
               만난 작은 양귀비 꽃을 그린거야. 그 옆에 작고 하얀 돌담으로 지어진
@@ -50,7 +58,9 @@ const postDetail: React.FC = () => {
             </span>
             <div>
               <div className={styles.hashTagContainer}>
-                <span className={styles.hashTagText2}>{detailData.hashTag}</span>
+                <span className={styles.hashTagText2}>
+                  {detailData.hashTag}
+                </span>
                 <Image
                   src={'/detailImage/apple.png'}
                   width={30}
@@ -62,7 +72,9 @@ const postDetail: React.FC = () => {
             </div>
           </div>
           <div className={styles.leftButtonContainer}>
-            <button className={styles.leftButton}>수정</button>
+            <button className={styles.leftButton} onClick={fixButtonHandler}>
+              수정
+            </button>
             <button className={styles.leftButton}>삭제</button>
           </div>
         </div>
